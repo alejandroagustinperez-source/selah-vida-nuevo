286c2f7964d5900b76f3f081b30be288cc0a3c11
Alejandro Perez <origenvitalsl@gmail.com>
2026-06-01 23:24:45 -0300

    Increase manuscript side panels from 72px to 140px

--- src/components/ManuscriptBorders.jsx
+++ src/components/ManuscriptBorders.jsx
@@ -5,18 +5,18 @@ export default function ManuscriptBorders({ children }) {
       <div
         className="hidden xl:block fixed top-0 bottom-0 left-0 pointer-events-none z-10 opacity-90"
         style={{
-          width: '72px',
+          width: '140px',
           background: "url('/border-ornament.png') repeat-y center top",
-          backgroundSize: '72px auto',
+          backgroundSize: '140px auto',
         }}
       />
       {/* Right ornamental panel (mirrored) */}
       <div
         className="hidden xl:block fixed top-0 bottom-0 right-0 pointer-events-none z-10 opacity-90"
         style={{
-          width: '72px',
+          width: '140px',
           background: "url('/border-ornament.png') repeat-y center top",
-          backgroundSize: '72px auto',
+          backgroundSize: '140px auto',
           transform: 'scaleX(-1)',
         }}
       />
